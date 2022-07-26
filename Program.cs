ac68dd34df73169c06ce4f58718088f4a30d5906
ViOs276 <tw01151@naver.com>
2022-07-27 06:02:03 +0900

    change to all random mac address

--- Program.cs
+++ Program.cs
@@ -23,13 +23,18 @@ namespace ChRouterMac
             var base64EncodedAuthenticationString = Convert.ToBase64String(Encoding.ASCII.GetBytes(authenticationString));
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
             httpClient.DefaultRequestHeaders.Referrer = new Uri($"http://{gateway}/cgi-bin/timepro.cgi?tmenu=iframe&smenu=hiddenwansetup");
-            var randomByte = new byte[1];
-            new Random().NextBytes(randomByte);
-            var randomByteString = string.Format("{0}", randomByte[0].ToString("X2"));
+            var randomBytes = GetRandomByte();
             var response = await httpClient.PostAsync($"http://{gateway}/cgi-bin/timepro.cgi?tmenu=main_frame&smenu=main_frame", 
-                new StringContent($"tmenu=iframe&smenu=hiddenwansetup&act=save&ocolor=&wan=wan1&ifname=eth2.2&nopassword=0&wan_type=dynamic&allow_private=on&fdns_dynamic1=1&fdns_dynamic2=1&fdns_dynamic3=1&fdns_dynamic4=1&sdns_dynamic1=1&sdns_dynamic2=0&sdns_dynamic3=0&sdns_dynamic4=1&dns_dynamic_chk=on&hw_dynamic1=2C&hw_dynamic2=F0&hw_dynamic3=5D&hw_dynamic4=37&hw_dynamic5=28&hw_dynamic6={randomByteString}&hw_conf_dynamic=on", Encoding.UTF8, "text/plain"));
+                new StringContent($"tmenu=iframe&smenu=hiddenwansetup&act=save&ocolor=&wan=wan1&ifname=eth2.2&nopassword=0&wan_type=dynamic&allow_private=on&fdns_dynamic1=1&fdns_dynamic2=1&fdns_dynamic3=1&fdns_dynamic4=1&sdns_dynamic1=1&sdns_dynamic2=0&sdns_dynamic3=0&sdns_dynamic4=1&dns_dynamic_chk=on&hw_dynamic1={string.Format("{0}", randomBytes[0].ToString("X2"))}&hw_dynamic2={string.Format("{0}", randomBytes[1].ToString("X2"))}&hw_dynamic3={string.Format("{0}", randomBytes[2].ToString("X2"))}&hw_dynamic4={string.Format("{0}", randomBytes[3].ToString("X2"))}&hw_dynamic5={string.Format("{0}", randomBytes[4].ToString("X2"))}&hw_dynamic6={string.Format("{0}", randomBytes[5].ToString("X2"))}&hw_conf_dynamic=on", Encoding.UTF8, "text/plain"));
             Console.WriteLine(response);
             Environment.Exit(0);
+        }
+
+        private static byte[] GetRandomByte()
+        {
+            var randomByte = new byte[6];
+            new Random().NextBytes(randomByte);
+            return randomByte;
         }
     }
 }
