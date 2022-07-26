d03a71a340f70e2a8a4b32dea79f5096ea2dab76
ViOs276 <tw01151@naver.com>
2022-07-27 05:40:51 +0900

    add signal when program finished

--- Program.cs
+++ Program.cs
@@ -28,6 +28,8 @@ namespace ChRouterMac
             var randomByteString = string.Format("{0}", randomByte[0].ToString("X2"));
             var response = await httpClient.PostAsync($"http://{gateway}/cgi-bin/timepro.cgi?tmenu=main_frame&smenu=main_frame", 
                 new StringContent($"tmenu=iframe&smenu=hiddenwansetup&act=save&ocolor=&wan=wan1&ifname=eth2.2&nopassword=0&wan_type=dynamic&allow_private=on&fdns_dynamic1=1&fdns_dynamic2=1&fdns_dynamic3=1&fdns_dynamic4=1&sdns_dynamic1=1&sdns_dynamic2=0&sdns_dynamic3=0&sdns_dynamic4=1&dns_dynamic_chk=on&hw_dynamic1=2C&hw_dynamic2=F0&hw_dynamic3=5D&hw_dynamic4=37&hw_dynamic5=28&hw_dynamic6={randomByteString}&hw_conf_dynamic=on", Encoding.UTF8, "text/plain"));
+            Console.WriteLine(response);
+            Environment.Exit(0);
         }
     }
 }
